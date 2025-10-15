f92ff395b26347bfa42f665cd4b3fee2822d5fa0
Matt Fitzpatrick <mattfitz@yahoo.com>
2025-10-15 10:53:01 -0700

    add unit test for search()

--- test.cpp
+++ test.cpp
@@ -8,3 +8,16 @@ TEST(TreeTest, NodeInitialization) {
     EXPECT_EQ(testNode->right, nullptr);
     delete testNode;
 }
+
+TEST(TreeTest, NodeSearch) {
+    Node* testTree = new Node(10);
+    testTree->left = new Node(5);
+    testTree->right = new Node(15);
+
+    EXPECT_NE(testTree->search(10), nullptr);
+    EXPECT_NE(testTree->search(5), nullptr);
+    EXPECT_NE(testTree->search(15), nullptr);
+    EXPECT_EQ(testTree->search(20), nullptr);
+
+    delete testTree;
+}
